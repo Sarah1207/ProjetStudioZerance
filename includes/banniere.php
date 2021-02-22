9ea1634e55e12eeafd7357d806d35d04ae37c701
Sarah HAMI <sarah.hami07@gmail.com>
2021-02-22 20:38:48 +0100

    CREATION

---
+++ includes/banniere.php
@@ -0,0 +1,9 @@
+
+
+<div id="banniere" class="text-center mx-auto">
+
+<p class="py-5"> Since 2016 </p>
+
+<h1> We create from <br/>scratch amazing <span class="yellow-line"> Shopify </span> </h1> 
+
+</div
---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<!-- USING BOOTSTRAP -->
+
+<div class="container-fluid">
+<header id="header-nav" class="row px-3 py-3">
+
+
+
+<div class="col-3">
+    <img alt="logo" src="assets/img/logo_studio-zerance-small.png" class="nav-logo">
+</div>
+
+
+<div class="col-9">
+    <ul class="nav-menu d-flex justify-content-end">
+        <li class="px-4 pt-3"> <a href="#"> Nos clients </a> </li>
+        <li class="px-4 pt-3"> <a href="#"> Notre offre </a> </li>
+        <li class="px-4 pt-3"> <a href="#"> Le Studio </a> </li>
+        <li class="px-4 pt-3"> <a href="#"> Contact </a> </li>
+    </ul>
+</div>
+
+
+</div>
+</header>
