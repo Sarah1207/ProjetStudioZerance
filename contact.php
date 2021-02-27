a2d372259590e689182f466c5a759ff758be290c
Sarah HAMI <sarah.hami07@gmail.com>
2021-02-27 19:10:38 +0100

    up

---
+++ contact.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Studio Zerance | We create from scratch amazing Shopify">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="canonical" href="https://studiozerance.fr/">
+    <link rel="shortcut icon" href="assets\img\logo_studio-zerance-small.png" type="image/png">
+    <meta name="author" content="Studio Zerance">
+    <title>Contact Studio Zerance</title>
+
+    
+    <link rel="stylesheet" href="assets/css/normalize.css">  
+    <link rel="stylesheet" href="assets/css/styleproject.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+
+</head>
+
+<body>
+
+<!-- header -->
+<?php require_once "includes/header.php" ?>
+
+
+
+<h1 class="pl-5"> Contact </h1>
+
+<form action="" method="post" class= "border-light p-5 w-25">
+  
+      <input type="text" class="form-control" placeholder="Prénom" required>
+      <br>
+      <input type="email" class="form-control" placeholder="Email" name="email" required>
+      <br>
+      <input type="textarea" class="form-control" placeholder="Message" name="message" required>
+  
+      <button type="submit" class="btn btn-primary mt-4">Valider</button>
+      <button type="button" class="btn btn-light mt-4 pl-5"> X </button>
+    </div>
+  </div>
+</form>
+
+
+</body>
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+
+
+
+</html>
